9d3cafdc026ff6d198964bb7da6ef5210bfed2ee
Meet <meetsarvaiya05@gmail.com>
2024-04-20 17:24:38 +0530

    twoday

---
+++ s6.py
@@ -0,0 +1,38 @@
+# list = [1,2,2,1]
+# list1 = list.copy()
+
+# list1.reverse()
+# if list==list1:
+#     print("paly")
+# else:
+#     print("not paly")
+
+# list = ['a','b','c','d']
+# list[0],list[2] = list[2],list[0] 
+# print(list)
+
+# list = ["a","b","c","d"]
+# i="c"
+# if i in list:
+#     print("pra...")
+# else:
+#     print("not pra...")
+
+# list = ["a","b","c","d"]
+# list1 = list
+# print(list1)
+
+# list = [1,2,3,4]
+# add=sum(list)
+# avg=add/len
+# print(add)
+# print(avg)
+
+# list = [1,2,3,4,5]
+# if len(list)==0:
+#     print("ip")
+# else:
+#     print("not ip")
+
+list = [1,2,3,4,3,4,2,4,3,2,3]
+print(tup.index[2])
